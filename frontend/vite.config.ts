2c841f3e3f92bc652961c12652709b0109d77599
Jafferjees-IT <25365485+Fakhruddins128@users.noreply.github.com>
2026-06-01 14:53:23 +0500

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        // target: "http://localhost:4000",
+        target: "https://allocation-msl6.onrender.com",
         changeOrigin: true
       }
     }
